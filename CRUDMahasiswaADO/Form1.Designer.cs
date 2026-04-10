0bef714b1cc1336b0b66273f5f60b7f8e725d895
Deva658 <devaaditya271@gmail.com>
2026-04-10 10:19:19 +0700

    menambahkan fungsi pada tombol menghapus data/delete data

--- CRUDMahasiswaADO/Form1.Designer.cs
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -192,6 +192,7 @@
             this.btnDelete.TabIndex = 16;
             this.btnDelete.Text = "Menghapus Data";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // Form1
             // 
