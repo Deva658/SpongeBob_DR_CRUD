0d564dacf6e1cbc9dca253a2a8d799e41bf862b2
Deva658 <devaaditya271@gmail.com>
2026-06-21 19:19:36 +0700

    mengubah class program

--- CRUDMahasiswaADO/Program.cs
+++ CRUDMahasiswaADO/Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CRUDMahasiswaADO
 {
-    internal static class Program
+    public partial class Program: Form
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public Program()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            InitializeComponent();
         }
     }
 }
