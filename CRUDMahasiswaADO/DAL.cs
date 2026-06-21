08dc998d8311d738ce4b63d18d8ca5a09b786049
Deva658 <devaaditya271@gmail.com>
2026-06-21 19:44:52 +0700

    menambahkan Function untuk mengambil IPAddress Server pada class DAL

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CRUDMahasiswaADO
 {
@@ -20,6 +21,28 @@ namespace CRUDMahasiswaADO
         SqlDataAdapter da;
         DataTable dtProdi;
         DataTable dtMahasiswa;
+
+        public static string GetLocalIPAddress()
+        {
+            string localIP = string.Empty;
+            try
+            {
+                var host = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName());
+                foreach (var ip in host.AddressList)
+                {
+                    if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        localIP = ip.ToString();
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error getting local IP address: " + ex.Message);
+            }
+            return localIP;
+        }
 
         public int CountMhs()
         {
